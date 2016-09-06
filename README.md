29aa24ccb72f0705d29c1efb356dc4b0e72a18cd
Krikor Boghossian <krikor.boghossian@gmail.com>
2016-09-06 19:13:30 +0300

    Initial upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Generic Player for the Radiowave template
+
+### MVC TEMPLATING
+The module already includes some generic CSS styling, which makes it truly 'plug and play' for your Joomla website. If you want more control, you can simply override both the generated HTML and CSS, using MVC template overrides within your Joomla template. Hover over each parameter in the module settings for additional info.
+
+### REQUIREMENTS
+- Joomla 2.5 or 3.x
+- The Radiowave Template
+
---
+++ mod_jw_radiowave_player.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @version     2.0
+ * @package     TinyLetter Subscribe (module)
+ * @author      JoomlaWorks - http://www.joomlaworks.net
+ * @copyright   Copyright (c) 2006 - 2013 JoomlaWorks Ltd. All rights reserved.
+ * @license     GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+// JoomlaWorks reference parameters
+$mod_name               = "mod_jw_radiowave_player";
+
+// Conventions
+if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
+
+// API
+$mainframe	= JFactory::getApplication();
+$document 	= JFactory::getDocument();
+
+// Assign paths
+$sitePath 	= JPATH_SITE;
+$siteUrl  	= substr(JURI::base(), 0, -1);
+
+// Module parameters
+$moduleclass_sfx 	= $params->get('moduleclass_sfx','');
+$stream_id 			= $params->get('stream_id','');
+$template 			= $params->get('template','Default');
+
+// Add the required behaviour.js file. The initialisation is done in the template.
+$document->addScript($siteUrl.'/modules/mod_jw_radiowave_player/includes/scripts/behaviour.js');
+
+// Output content with template
+require(JModuleHelper::getLayoutPath($mod_name,$template.DS.'default'));
+
+// END
