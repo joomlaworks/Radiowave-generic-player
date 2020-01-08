314022bd0c287deedf0fde777f5f682f87d0d842
Fotis Evangelou <fevangelou@users.noreply.github.com>
2020-01-08 15:24:55 +0200

    Remove references to "javascript:;"

--- tmpl/Default/default.php
+++ tmpl/Default/default.php
@@ -19,12 +19,12 @@ defined('_JEXEC') or die;
             <div class="jp-gui jp-interface">
                 <ul class="jp-controls">
                     <li>
-                        <a href="javascript:;" style="display:block;" class="jp-play" title="Play"><i class="icon-play"></i><span><?php echo JText::_('MOD_JW_RADIOWAVE_LISTEN_LIVE'); ?></span></a>
-                        <a href="javascript:;" style="display:none;" class="jp-pause" title="Pause"><i class="icon-pause"></i><span><?php echo JText::_('MOD_JW_RADIOWAVE_PAUSE'); ?></span></a>
+                        <a href="#play" style="display:block;" class="jp-play" title="Play"><i class="icon-play"></i><span><?php echo JText::_('MOD_JW_RADIOWAVE_LISTEN_LIVE'); ?></span></a>
+                        <a href="#pause" style="display:none;" class="jp-pause" title="Pause"><i class="icon-pause"></i><span><?php echo JText::_('MOD_JW_RADIOWAVE_PAUSE'); ?></span></a>
                     </li>
                     <li>
-                        <a href="javascript:;" style="display:block;" class="jp-mute" title="Mute"><i class="icon-volume-up"></i></a>
-                        <a href="javascript:;" style="display:none;" class="jp-unmute" title="Unmute"><i class="icon-volume-off"></i></a>
+                        <a href="#up" style="display:block;" class="jp-mute" title="Mute"><i class="icon-volume-up"></i></a>
+                        <a href="#down" style="display:none;" class="jp-unmute" title="Unmute"><i class="icon-volume-off"></i></a>
                     </li>
                     <li>
                         <a class="popup" href="index.php?tmpl=popup" onclick="window.open(this.href,'targetWindow','width=690,height=600,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=yes,');return false;"><i class="icon-popup"></i></a>
